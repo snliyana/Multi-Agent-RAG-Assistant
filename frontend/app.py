2bff180a9bf25e92b62ed0e007b895428c6f3064
Liyanaishak <snliyana00@gmail.com>
2026-09-08 11:20:23 +0800

    Fix Streamlit backend API connection

--- frontend/app.py
+++ frontend/app.py
@@ -4,9 +4,12 @@ import requests
 import streamlit as st
 
 
-API_URL = os.getenv(
+API_URL = st.secrets.get(
     "API_URL",
-    "http://127.0.0.1:8000"
+    os.getenv(
+        "API_URL",
+        "http://127.0.0.1:8000"
+    )
 )
 
 # --------------------------------
